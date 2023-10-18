da97cdc8d34238dd6d8972ed6a90cb2d691cf6b9
eariasp <eariasp@unal.edu.co>
2023-10-17 23:23:17 -0500

    First commit, adding all the current files in the local directory

---
+++ include/get_data.py
@@ -0,0 +1,7 @@
+from data_download import Download_LE_Data, Download_SE_Data
+
+firstYear = -1999
+lastYear = 2001
+
+Download_LE_Data.iterate(firstYear,lastYear)
+Download_SE_Data.iterate(firstYear,lastYear)
